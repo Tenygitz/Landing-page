5f26eba526ff059bdc7ee5e7956a88b74e8d23f1
tenygitz <tenybits@gmail.com>
2022-06-06 17:11:34 +0530

    final message

---
+++ js/main.js
@@ -0,0 +1,8 @@
+ // mobilemenu
+ let icon=document.querySelector(".fa-bars");
+ let menu=document.querySelector(".menu");
+
+ icon.addEventListener("click",()=>{
+     menu.classList.toggle("open");
+     console.log("teny")
+ })
